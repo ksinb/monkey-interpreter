349b987642629f6236a1fedeb18b71493575d773
ksinb <61133806+ksinb@users.noreply.github.com>
2017-02-07 17:29:28 +0900

    Implement bindings and environment

--- src/repl/repl.go
+++ src/repl/repl.go
@@ -7,12 +7,14 @@ import (
 	"io"
 	"lexer"
 	"parser"
+	"object"
 )
 
 const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Println(PROMPT)
@@ -31,7 +33,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
